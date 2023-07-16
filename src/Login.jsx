4fae1f968eb4637f46da50969505f07f1f5ced21
BilalHammi <Bilalhammi45@gmail.com>
2023-07-16 16:27:32 +0200

    Added "See password" back-end so user see password

--- src/Login.jsx
+++ src/Login.jsx
@@ -3,6 +3,14 @@ import Googlebutton from './Googlebutton';
 import './index.css'
 
 export const Login = () => {
+  const show = () => {
+    var x = document.getElementById("password");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+  }
   return (
     <>
       <div className='container mx-auto flex justify-center'>
@@ -17,7 +25,7 @@ export const Login = () => {
                   <input type="text" name='E-mail' placeholder='E-mail' className='mt-14 w-3/4 rounded-lg bg-white text-center py-2 font-bold border-2 border-black' required />
                 </div>
                 <div className='flex justify-center'>
-                  <input type="password" name='Password' placeholder='Password' className='mt-12 w-3/4 rounded-lg bg-white text-center py-2 font-bold border-2 border-black' required />
+                  <input type="password" name='password' id="password" placeholder='Password' className='mt-12 w-3/4 rounded-lg bg-white text-center py-2 font-bold border-2 border-black' required />
                 </div>
                 <div className='flex justify-center'>
                   <input type="submit" name='Submit' placeholder='Submit' value="Submit" className='mt-10 w-3/4 rounded-lg bg-gray-600 text-center py-2 font-bold border-2 border-black hover:bg-black hover:text-white' />
@@ -26,7 +34,7 @@ export const Login = () => {
               <div className="flex justify-center mt-3 text-white font-bold">
                 <div className='flex flex-row'>
                   <p className="pr-2">See password?</p>
-                  <input type="checkbox" id="Check1" className='' />
+                  <input type="checkbox" id="Check1" className='' onClick={show} />
                 </div>
               </div>
               <p className="mt-3 underline underline-offset-2 flex justify-center text-white font-bold">Forgot Password?</p>
@@ -35,7 +43,7 @@ export const Login = () => {
                   <Link to="/register" className="text-blue-400 font-bold ml-2 underline underline-offset-2">Click here!</Link></p>
               </div>
               <div className="flex">
-                <p className="w-full text-center my-2 bg-blue px-1">Or</p>
+                <p className="w-full text-center my-2 bg-blue px-1 font-bold">Or</p>
               </div>
               <Googlebutton />
             </form>
